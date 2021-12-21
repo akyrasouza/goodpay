cf5e229a25cdcc6c2a8db70930375c5359848b7f
akyrasouza <75281221+akyrasouza@users.noreply.github.com>
2021-12-21 13:59:10 -0300

    implementação do modal de comunicação

--- src/index.js
+++ src/index.js
@@ -8,15 +8,12 @@ const entrar = document.getElementById("btnEntrar");
 entrar.addEventListener("click", (event) => {
     let user= document.getElementById("usuario").value,
     senha = document.getElementById("senha").value;
+    let alertModal = controlerUserLogin.loginInfo(user,senha); 
+    document.getElementById("titleModal").innerHTML = alertModal.title;
+    document.getElementById("bodyModal").innerHTML = alertModal.bodyModal;
+    document.getElementById("btnModalClose").innerHTML = alertModal.b1;
+    document.getElementById("btnModalSave").innerHTML = alertModal.b2;
 
-    //Testando se os campo não estão vazios.
-    if(!user|| user=== null || user==="" || !senha || senha === null || senha===""){
-        alert("Campos vazios, não há como executar o login");
-        document.getElementById("titleModal").innerHTML = "Campos vazios";
-        document.getElementById("bodyModal").innerHTML = "Os Campos não podem servazios";
-        document.getElementById("btnModalClose").innerHTML = "Fechar";
-        document.getElementById("btnModalSave").innerHTML = "Salvar";
-        myModal.show();
-    } else {
-        controlerUserLogin.loginInfo(user,senha);    }
-})
+    myModal.show();
+
+});
